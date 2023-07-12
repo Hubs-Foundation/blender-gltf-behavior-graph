292bad39a4393fc1fab4720e90767f7a9f50d1fb
Manuel Martin <manmartin@mozilla.com>
2023-07-12 15:24:06 +0200

    The Hubs registry handles de-registration

--- hubs_components.py
+++ hubs_components.py
@@ -321,10 +321,6 @@ def do_register(ComponentClass):
     register_component(ComponentClass)
     __components_registry[ComponentClass.get_name()] = ComponentClass
 
-def do_unregister(ComponentClass):
-    unregister_component(ComponentClass)
-    __components_registry.pop(ComponentClass.get_name())
-
 def register():
     do_register(Grabbable)
     do_register(RigidBody)
@@ -338,12 +334,8 @@ def register():
 
 
 def unregister():
-    do_unregister(Grabbable)
-    do_unregister(RigidBody)
-    do_unregister(PhysicsShape)
-
+    # No need to unregister the Hubs components here, the registry will handle that.
     bpy.utils.unregister_class(CustomTagItem)
     bpy.utils.unregister_class(CUSTOM_TAGS_UL_tags_list)
     bpy.utils.unregister_class(CUSTOM_TAGS_OT_add_tag)
     bpy.utils.unregister_class(CUSTOM_TAGS_OT_remove_tag)
-    do_unregister(CustomTags)
